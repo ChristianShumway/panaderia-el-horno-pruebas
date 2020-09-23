dbe2233fd4139a61d8d2602e20951da126469345
Christian Torres <christiantorres084@gmail.com>
2020-09-23 14:30:54 -0500

    proyecto listo para desarrollar

---
+++ src/app/shared/services/autenticacion.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from './../../../environments/environment';
+import { Usuario } from './../models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+
+  private currentUserSubject: BehaviorSubject<Usuario>;
+  public currentUser: Observable<Usuario>;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.currentUserSubject = new BehaviorSubject<Usuario>(JSON.parse(localStorage.getItem('currentUserSession')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): Usuario {
+    return this.currentUserSubject.value;
+  }
+
+  loginUser(user) {
+    const headerss = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(`${environment.apiURL}/dashboard/autenticacion`, JSON.stringify(user), { headers: headerss })
+      .pipe(map(user => {
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('currentUserSession', JSON.stringify(user.idUsuario));
+        this.currentUserSubject.next(user.idUsuario);
+        return user;
+      }));
+  }
+
+  logout() {
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('currentUserSession');
+    this.currentUserSubject.next(null);
+  }
+
+  userAuthenticated(idPerfil: number, idOpcion: number) {
+    return this.http.get(`${environment.apiURL}/config/isAuthenticated/${idPerfil}/${idOpcion}`);
+  }
+
+  restoreUser(user) {
+    const headerss = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(`${environment.apiURL}/user/restoreUser`, JSON.stringify(user), { headers: headerss })
+  }
+
+  updatePassword(user) {
+    const headerss = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(`${environment.apiURL}/user/changePassword`, JSON.stringify(user), { headers: headerss })
+  }
+
+
+}
+
---
+++ src/app/views/login/components/restaurar-password/restaurar-password.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatProgressBar, MatButton } from '@angular/material';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomValidators } from 'ng2-validation';
+
+import { AutenticacionService } from './../../../../shared/services/autenticacion.service';
+// import { UsuariosService } from './../../../../shared/services/usuarios.service';
+import { Usuario } from '../../../../shared/models/usuario';
+
+@Component({
+  selector: 'app-restaurar-password',
+  templateUrl: './restaurar-password.component.html',
+  styleUrls: ['./restaurar-password.component.scss']
+})
+export class RestaurarPasswordComponent implements OnInit {
+
+  @ViewChild(MatProgressBar, { static: false }) progressBar: MatProgressBar;
+  @ViewChild(MatButton, { static: false }) submitButton: MatButton;
+
+  passwordForm: FormGroup;
+  returnUrl: string;
+  formData = {};
+  existeUsuario= false;
+  usuario: Usuario;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
+    private autenticacionService: AutenticacionService,
+    // private usuariosService: UsuariosService
+  ) {
+   
+  }
+
+  ngOnInit() {
+    this.getValidations();
+    this.getUser();
+  }
+
+  getValidations() {
+    let contrasena = new FormControl('', [Validators.required, Validators.minLength(8), Validators.required]);
+    let confirmarContrasena = new FormControl('', CustomValidators.equalTo(contrasena));
+
+    this.passwordForm = new FormGroup({
+      contrasena: contrasena,
+      confirmarContrasena: confirmarContrasena,
+    })
+  }
+
+  getUser(){
+    this.route.params.subscribe( (data: Params) => {
+      if(data.id){
+        console.log(data);
+        // this.usuariosService.getUsuario(data.id).subscribe(
+        //   (user: Usuario) => {
+        //     this.usuario = user;
+        //     if(this.usuario.cambiarContrasena === 0) {
+        //       this.router.navigate(['/login']);
+        //     }
+        //   },
+        //   error => console.log(error)
+        // );
+      }
+    })
+  }
+
+  restorePassword() {
+    if(this.passwordForm.valid){
+      const usuario = {
+        idUsuario: this.usuario.idUsuario,
+        ...this.passwordForm.value,
+      };
+      console.log(usuario);
+      this.submitButton.disabled = true;
+      this.progressBar.mode = 'indeterminate';
+      this.autenticacionService.updatePassword(usuario).subscribe(
+        success => {
+          console.log(success);
+          if(success.estatus === '05'){
+            this.useAlerts(success.mensaje, ' ', 'success-dialog');
+            this.autenticacionService.logout();
+            this.router.navigate(['/login']);
+          }
+        },
+        error => {
+          console.log(error);
+          this.useAlerts('Contraseña no se ha cambiado', ' ', 'error-dialog');
+        }
+      );
+    }
+  }
+
+  useAlerts(message, action, className) {
+    this.snackBar.open(message, action, {
+      duration: 4000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'right',
+      panelClass: [className]
+    });
+  }
+
+}
